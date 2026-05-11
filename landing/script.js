73443b53e3b688deb170152b8e429216f7fe9bf0
savavyalkov <juggson08@gmail.com>
2026-05-11 21:27:04 +0300

    landing

--- landing/script.js
+++ landing/script.js
@@ -1,15 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const themeBtn = document.querySelector(".theme-btn");
 
-    themeBtn.addEventListener("click", () => {
-        document.body.classList.toggle("dark-theme");
-        localStorage.setItem(
-            "theme",
-            document.body.classList.contains("dark-theme") ? "dark" : "light"
-        );
-    });
+  const themeBtn = document.querySelector(".theme-btn");
 
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark-theme");
-    }
+  themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    localStorage.setItem(
+      "theme",
+      document.body.classList.contains("dark-theme") ? "dark" : "light"
+    );
+  });
+
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-theme");
+  }
+
+  const loginBtn = document.querySelector(".btn-1");
+  const registerBtn = document.querySelector(".btn-2");
+  const joinBtn = document.querySelector(".btn-3");
+  const register2Btn = document.querySelector(".btn-4");
+
+  loginBtn.addEventListener("click", () => {
+    window.location.href = "./pages/login.html";
+  });
+
+  registerBtn.addEventListener("click", () => {
+    window.location.href = "./pages/register.html";
+  });
+
+  joinBtn.addEventListener("click", () => {
+  window.location.href = "./pages/register.html";
+  });
+  register2Btn.addEventListener("click", () => {
+  window.location.href = "./pages/register.html";
+  });
+
 });
