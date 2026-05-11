5933997a7b7e2a1bacb1a51f28f1b0ad61fc20c5
savavyalkov <juggson08@gmail.com>
2026-05-11 14:42:41 +0300

    update

---
+++ landing/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const themeBtn = document.querySelector(".theme-btn");
+
+  themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    localStorage.setItem(
+      "theme",
+      document.body.classList.contains("dark-theme") ? "dark" : "light"
+    );
+  });
+
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-theme");
+  }
+});
